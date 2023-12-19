56a5562c7533ee16019e1c384eb9cc8c7753bc6a
kim <xiao.kim@tradego8.com>
2023-12-19 17:59:45 +0800

    模板文件修改

--- mysite/ai/urls.py
+++ mysite/ai/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('resume', views.resume, name='resume'),
+    path('works', views.works, name='works'),
+    path('blog', views.blog, name='blog'),
+    path('contact', views.contact, name='contact'),
+
+
 ]
--- mysite/ai/views.py
+++ mysite/ai/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 主页
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'index'
+    }
+    return render(request, 'index.html', context)
+
+# 简介
+def resume(request):
+    context = {
+        'name': 'resume'
+    }
+    return render(request, 'resume.html', context)
+
+# 工作经历
+def works(request):
+    context = {
+        'name': 'works'
+    }
+    return render(request, 'works.html', context)
+
+# 博客
+def blog(request):
+    context = {
+        'name': 'index'
+    }
+    return render(request, 'blog.html', context)
+
+# 联系
+def contact(request):
+    context = {
+        'name': 'contact'
+    }
+    return render(request, 'contact.html', context)
